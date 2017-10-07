a75c872bac8624f4fd72cbcda645b658836ef358
Daniel Lapinski <daniellapinski9@gmail.com>
2017-10-06 21:02:12 -0400

    finished
    Succeesss!!!!!!!

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-$("#submit").click(()=>{
-  const myvar = $("#name1").val();
-  alert("#firstname")
+$("button#yo").click(()=>{
+
+  // jquery syntax works like this
+  // $("thingtoget").stufftodo()
+
+  // instead of alerting, do this:
+  // get a thing on the page, change the text
+
+  const myvar = $("input#name1").val();
+  const urvar = $("input#name2").val();
+  $("p#display").append(myvar + " " + urvar);
 })
