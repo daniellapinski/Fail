a11bcef76226e9582a8d5c9e1b39ef88e50f91f3
Daniel Lapinski <daniellapinski9@gmail.com>
2017-11-03 19:09:31 -0400

    S
    S

--- challenge5.js
+++ challenge5.js
@@ -1,4 +1,22 @@
-$("#life").click(function(){
-    $("p#display").append("#herro");
-    console.log("wassup")
-});
+function why() {
+  const myInt = "7"
+  $("body").append(`${myInt}`)
+    $("#terble").append(`<tr>
+    <th>myInt:</th>
+    <th>myString:</th>
+    <th>myNumString:</th></tr>  <tr>
+    <td>7</td>
+    <td>the number 7</td>
+    <td>7s</td>
+  </tr>`);
+  $("#terble2").append(`<tr>
+  <th>Addition</th>
+  <th>myInt+myInt?</th>
+  <th>myInt+myString?</th></tr>  <tr>
+  <td></td>
+  <td>14</td>
+  <td>7 the number 7</td>
+</tr>`)
+console.log("The code works");
+};
+   why();
