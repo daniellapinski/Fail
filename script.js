d70ed9774b8c6b4d72253c0523d33c244cfed7ff
Daniel Lapinski <daniellapinski9@gmail.com>
2017-11-07 15:44:01 -0500

    progress
    yayay

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $("button#yo").click(()=>{
   const myvar = $("input#name1").val();
   const urvar = $("input#name2").val();
-  $("p#display").append(myvar + " " + urvar);
+  $("p#display").append(myvar + " " + urvar + " ");
 })
