f7d4aeeef6f082d47e746f0181c0bd33ea0d1cc3
Daniel Lapinski <daniellapinski9@gmail.com>
2017-12-01 18:43:09 -0500

    h
    H

--- challenge5.js
+++ challenge5.js
@@ -1,81 +1,97 @@
+var myInt = 4;
+var myString = " the number 4 ";
+var myNumString = "4";
+
+
 $("#challenge5vars").append(`
   <table>
     <tr>
-      <th>myInt:</th>
-      <td></td>
+      <td class="qa">myInt:</td>
+      <td>${myInt}</td>
     </tr>
     <tr>
-      <th>myString:</th>
-      <td></td>
+      <td class="qa">myString:</td>
+      <td>${myString}</td>
     </tr>
     <tr>
-      <th>myNumString:</th>
-      <td></td></tr>
+      <td class="qa">myNumString:</td>
+      <td>${myNumString}</td></tr>
   </table>`);
 $("#challenge5predictions").append(`
   <table>
     <tr>
-      <th><b>Addition</b</th>
-      <td></td>
+      <th class="mathtype" colspan=2><b>Addition</b></th>
     </tr>
     <tr>
-      <th>myInt+myInt?</th>
-      <td></td>
+      <td class="qa">myInt+myInt?</td>
+      <td>${myInt+myInt}</td>
     </tr>
     <tr>
-      <th>myInt+myString?</th>
-      <td></td>
+      <td class="qa">myInt+myString?</td>
+      <td>${myInt+myString}</td>
     </tr>
     <tr>
-      <th>myInt+myNumString?</th>
-      <td></td>
+      <td class="qa">myInt+myNumString?</td>
+      <td>${myInt+myNumString}</td>
     </tr>
     <tr>
-      <th>myNumString+myInt?</th>
-      <td></td>
+      <td class="qa">myNumString+myInt?</td>
+      <td>${myNumString+myInt}</td>
     </tr>
     <tr>
-      <th><b>Fancier addition</b></th>
-      <td></td>
+      <th class="mathtype" colspan=2><b>Fancier addition</b></th>
     </tr>
     <tr>
-      <th>myInt+myNumString+myString?</th>
-      <td></td>
+      <td class="qa">myInt+myNumString+myString?</td>
+      <td>${myInt+myNumString+myString}</td>
     </tr>
     <tr>
-      <th>myInt+myInt+myString?</th>
-      <td></td>
+      <td class="qa">myInt+myInt+myString?</td>
+      <td>${myInt+myInt+myString}</td>
     </tr>
      <tr>
-      <th>myString+myInt+myInt?</th>
-      <td></td>
+      <td class="qa">myString+myInt+myInt?</td>
+      <td>${myString+myInt+myInt}</td>
     </tr>
     <tr>
-      <th><b>Subtraction</b></th>
-      <td></td>
+      <th class="mathtype" colspan=2><b>Subtraction</b></th>
     </tr>
     <tr>
-      <th>myInt-myString?</th>
-      <td></td>
+      <td class="qa">myInt-myString?</td>
+      <td>${myInt-myString}</td>
     </tr>
    <tr>
-      <th>myInt-myNumString?</th>
-      <td></td>
+      <td class="qa">myInt-myNumString?</td>
+      <td>${myInt-myNumString}</td>
   </tr>
    <tr>
-      <th>myString-myNumString</th>
-      <td></td>
+      <td class="qa">myString-myNumString?</td>
+      <td>${myString-myNumString}</td>
   </tr>
    <tr>
-      <th><b>Comparisons</b></th>
-      <td></td>
+      <th class="mathtype" colspan=2><b>Comparisons</b></th>
   </tr>
    <tr>
-      <th>myInt==myNumString?</th>
-      <td></td>
+      <td class="qa">myInt==myNumString?</td>
+      <td>${myInt==myNumString}</td>
   </tr>
   <tr>
-       <th>myInt===myNumString</th>
-       <td></td>
-  </tr>   
+       <td class="qa">myInt===myNumString</td>
+       <td>${myInt===myNumString}</td>
+  </tr>
+  <tr>
+       <th class="mathtype" colspan=2><b>Multiplication</b></th>
+  </tr>
+  <tr>
+       <td class="qa">myInt*myInt?</td>
+       <td>${myInt*myInt}</td>
+  </tr>
+  <tr>
+       <td class="qa">myInt*myNumString?</td>
+       <td>${myInt*myNumString}</td>
+  </tr>
+  <tr>
+       <td class="qa">myInt*myString?</td>
+       <td>${myInt*myString}</td>
+  </tr>
   </table>`);
---
+++ practice.js
@@ -0,0 +1,15 @@
+function mynum() {
+    var num = 20;
+    var day = "monday";
+    if (num < 7) {
+        alert("Math");
+    } else if (day=="meow") {
+        alert("Yaarit");
+    } else if (day=="monday"){
+        alert("FPLRHH");
+    } else {
+        alert(day);
+    }
+}
+mynum();
+console.log()
